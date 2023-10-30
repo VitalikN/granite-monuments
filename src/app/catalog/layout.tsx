8bd2a140fda31e3108ca3f20c3d2b3bfd2b49589
vitalik.nozhenko@gmail.com <109283146+VitalikN@users.noreply.github.com>
2023-10-30 21:46:56 +0200

    fix  Deploy

--- src/app/catalog/layout.tsx
+++ src/app/catalog/layout.tsx
@@ -1,7 +1,7 @@
 import Product from "@/components/Product";
 import { CatalogLayoutProps } from "@/types/types";
 
-const CatalogLayout: React.FC<CatalogLayoutProps> = ({ children }) => {
+export const CatalogLayout: React.FC<CatalogLayoutProps> = ({ children }) => {
   return (
     <>
       <Product />
